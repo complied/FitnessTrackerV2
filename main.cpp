12ff9e99d61895cba6dc5c595a773cf0f40cc95b
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-02 12:26:25 -0700

    main updated with new display code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,66 @@
+//
+//  main.cpp
+//  Fitness Tracker
+//
+//  Created by Subigya Parajuli on 2/10/25.
+//
+
 #include <iostream>
+using namespace std;
+#include <iomanip>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+#include "include/DisplayMenu.h"
+#include "include/calorieChecker.h"
+#include "include/workoutReccomendation.h"
+#include "include/workoutLogger.h"
+#include "include/workoutAssign.h"
+#include "include/fileManager.h"
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+
+// ****** Updated All these, Included every hpp function in main ******
+
+int main(int argc, const char * argv[]) {
+    int choice;
+
+    while (true) { // Loop until the user chooses to exit
+        displayMenu::displayMenu(); // Function calling in main - now works!
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        // Input validation
+        if (cin.fail()) {
+            cin.clear();  // Clears the error flag
+            cin.ignore();  // Clears input buffer
+            cout << "Invalid input! Please Re-Enter a number: ";
+            cin >> choice;
+        }
+
+        switch (choice) {
+            case 1:
+                calorieChecker::calorieChecker();
+                break;
+            case 2:
+                workoutReccomendation::workoutReccomendation(); // Workout Recommender function here now
+                break;
+            case 3:
+                logWorkout::logWorkout(logWorkout::activities, logWorkout::durations, logWorkout::workoutCount);
+                break;
+            case 4:
+                workoutManager::assignWorkoutToDay(logWorkout::activities, logWorkout::durations, logWorkout::workoutCount); // connected both - logged workout to workout manager
+                break;
+            case 5:
+                cout << "Exiting program. Thank you for using the fitness tracker!" << endl;
+                return 0;
+            default:
+                cout << "Invalid choice! Please enter a number between 1 and 5." << endl;
+        }
+
+
+        cout << setfill(' ') << setw(10) << ""<< "Please Press Enter to return to the main menu.";
+        cin.ignore(); // Clears input buffer
+        cin.get(); // Waits for Enter key - Waits for any characters with white spaces for examples enter or return!
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
