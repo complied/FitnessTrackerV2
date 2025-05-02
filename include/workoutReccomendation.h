25f63eb5c7bfaae251dee82c2e8adfd4e4a798ba
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-02 13:21:25 -0700

    updated code with map enum and struct implementation

--- include/workoutReccomendation.h
+++ include/workoutReccomendation.h
@@ -1,15 +1,37 @@
+
 #ifndef Utils_wworkoutReccomendation
 #define Utils_wworkoutReccomendation
 
 // Included all the necesary libraries
 #include <iostream>
-using namespace std;
 #include <iomanip>
 #include <string>
+#include <vector> // swapped from ararys to vector for better usauge
+#include <map> // using for .at - for picking difficulty
 
-namespace workoutReccomendation{
-    void workoutReccomendation(); // defined function in the hpp file!
+using namespace std;
+
+namespace workoutReccomendation {
+
+    // now creating enum workout class for different workouts
+
+    enum class Difficulty{
+        Beginner =1,
+        Moderate = 2,
+        InterMediate = 3
+
+    };
+
+    struct WorkoutDay {
+        string name;               // Workout day label
+        vector<string> exercises;  // Exercises for this day
+    };
+
+    void workoutReccomendation();
+
 }
+
+
 # endif
 
 
