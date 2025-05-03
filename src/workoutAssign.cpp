4e1300deae915cdb82a9d4052d7420f33e22db51
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-02 18:49:40 -0700

    updated workout Asssign which links with workout logger

--- src/workoutAssign.cpp
+++ src/workoutAssign.cpp
@@ -2,22 +2,22 @@
 #include "../include/workoutLogger.h" // Including workoutLogger to access activities and durations
 
 using namespace std;
+using namespace logWorkout; // Access activities, durations globally
 
 namespace workoutManager {
     map<string, vector<shared_ptr<workout>>> workoutManager;
 
     workout::workout(const string &t, double d) : type(t), duration(d) {} // workout constructor
 
-
-    void assignWorkoutToDay(string activities[], double durations[], int workoutCount) {
-        for (int i = 0; i < workoutCount; i++) { // switching through each workout
+    void assignWorkoutToDay() {
+        for (size_t i = 0; i < activities.size(); ++i) { // switching through each workout
             string day;
 
             // assigning workout activity to selected day
             cout << "Please Enter a day you want to assign the workout: [ " << activities[i] << " ] ";
             cin >> day;
 
-            if (workoutManager[day].size() >= 5) { // limiting maximym to 5
+            if (workoutManager[day].size() >= 5) { // limiting maximum to 5
                 cout << "Day is full! Cannot assign more workouts to " << day << ".\n";
                 continue;
             }
@@ -29,6 +29,15 @@ namespace workoutManager {
             workoutManager[day].push_back(workoutPtr);
 
             cout << "Perfect! Your Workout [" << activities[i] << "] has been assigned to: " << day << "\n";
+        }
+
+        // displaying the whole workout map
+        cout << "\nWorkout Assignment Schedule:\n";
+        for (const auto& pair : workoutManager) {
+            cout << pair.first << ":\n";
+            for (const auto& workoutPtr : pair.second) {
+                cout << "  - " << workoutPtr->type << " (" << workoutPtr->duration << " min)\n";
+            }
         }
     }
 }
