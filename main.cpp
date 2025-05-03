8214a3d45f062e24003add9863089272b95c9666
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-02 20:04:15 -0700

    Patched small user based bug

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
-//
-//  main.cpp
+// main.cpp
 //  Fitness Tracker
-//
 //  Created by Subigya Parajuli on 2/10/25.
 //
 
 #include <iostream>
-using namespace std;
 #include <iomanip>
+#include <limits>  // for numeric_limits
+using namespace std;
 
 #include "include/DisplayMenu.h"
 #include "include/calorieChecker.h"
@@ -16,8 +15,7 @@ using namespace std;
 #include "include/workoutAssign.h"
 #include "include/fileManager.h"
 
-
-// ****** Updated All these, Included every hpp function in main ******
+// ****** Updated All these: removed argument interface for logWorkout and adjusted assignment call ******
 
 int main(int argc, const char * argv[]) {
     int choice;
@@ -31,36 +29,39 @@ int main(int argc, const char * argv[]) {
         // Input validation
         if (cin.fail()) {
             cin.clear();  // Clears the error flag
-            cin.ignore();  // Clears input buffer
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Clears input buffer
             cout << "Invalid input! Please Re-Enter a number: ";
             cin >> choice;
         }
 
         switch (choice) {
             case 1:
-                calorieChecker::calorieChecker();
+                calorieChecker::emergencyTracker(); // Emergency tracker using unique_ptr
                 break;
             case 2:
                 workoutReccomendation::workoutReccomendation(); // Workout Recommender function here now
-                break;
+                continue;
             case 3:
-                logWorkout::logWorkout(logWorkout::activities, logWorkout::durations, logWorkout::workoutCount);
+                logWorkout::logWorkout(); // Updated
                 break;
             case 4:
-                workoutManager::assignWorkoutToDay(logWorkout::activities, logWorkout::durations, logWorkout::workoutCount); // connected both - logged workout to workout manager
+                workoutManager::assignWorkoutToDay(); // connected both - logged workout to workout manager
                 break;
             case 5:
+                fileManager::readEmergencySessionsFromBinary();
+                break;
+            case 6:
                 cout << "Exiting program. Thank you for using the fitness tracker!" << endl;
                 return 0;
             default:
                 cout << "Invalid choice! Please enter a number between 1 and 5." << endl;
         }
 
-
-        cout << setfill(' ') << setw(10) << ""<< "Please Press Enter to return to the main menu.";
-        cin.ignore(); // Clears input buffer
+        cout << setfill(' ') << setw(10) << "" << "Please Press Enter to return to the main menu.";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears input buffer
         cin.get(); // Waits for Enter key - Waits for any characters with white spaces for examples enter or return!
     }
 
     return 0;
 }
+
